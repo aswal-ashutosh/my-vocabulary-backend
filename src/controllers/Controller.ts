d5f6555e6ebd55d8c845f985f56d90023ee6813a
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-05 20:48:07 +0530

    add base class for all controller classes

---
+++ src/controllers/Controller.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { RequestValidationSchema } from "../types";
+import Joi from "joi";
+import HttpError from "../customs/HttpError";
+import HttpStatusCode from "../constants/http-status-codes";
+
+export default abstract class Controller {
+    constructor(private readonly req: Request, private readonly res: Response) {}
+
+    sendResponse(status: number, body?: any) {
+        if (body) {
+            this.res.status(status).json(body);
+        } else {
+            this.res.sendStatus(status);
+        }
+    }
+
+    handleError(error: Error | HttpError) {
+        if (!(error instanceof HttpError)) {
+            error = HttpError.fromError(error);
+        }
+        const { message, statusCode } = error as HttpError;
+        this.res.status(statusCode).json({ error: message });
+    }
+
+    async validateRequest({
+        bodySchema = Joi.any(),
+        paramsSchema = Joi.any(),
+        querySchema = Joi.any(),
+    }: RequestValidationSchema = {}) {
+        try {
+            const schema = Joi.object({ body: bodySchema, params: paramsSchema, query: querySchema });
+            const { body, params, query } = this.req;
+            return await schema.validateAsync({ body, params, query });
+        } catch (error: any) {
+            if (error instanceof Joi.ValidationError) {
+                throw new HttpError({ message: error.details[0].message, statusCode: HttpStatusCode.BAD_REQUEST });
+            }
+            throw error;
+        }
+    }
+}
