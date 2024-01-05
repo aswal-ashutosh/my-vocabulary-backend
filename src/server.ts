35161935d66738a82b2b08efbd1a24b647ffa516
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-05 20:48:43 +0530

    add error-handling middleware

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
-import express from "express";
-// import cors from "cors";
+import express, { Request, Response, NextFunction } from "express";
 import { https } from "firebase-functions";
+import HttpStatusCode from "./constants/http-status-codes";
 
 const server = express();
 
 server.get("/", (req, res, next) => {
     res.status(200).json({ message: "Hello from firebase functions." });
+});
+
+server.use((error: Error, req: Request, res: Response, next: NextFunction) => {
+    res.status(HttpStatusCode.INTERNAL_SERVER_ERROR).json({ error: `UNCAUGHT_ERROR: ${error.message}` });
 });
 
 server.listen(3000);
