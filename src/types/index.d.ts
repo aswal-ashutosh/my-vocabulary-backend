8643050101372ab4e12af8b69ece619f99d8d31c
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-05 23:28:43 +0530

    add type ValidatedRequestData | APIResponse

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,7 +1,18 @@
 import Joi from "joi";
 
 export interface RequestValidationSchema {
-    bodySchema?: Joi.AnySchema,
-    paramsSchema?: Joi.AnySchema,
-    querySchema?: Joi.AnySchema,
+    bodySchema?: Joi.AnySchema;
+    paramsSchema?: Joi.AnySchema;
+    querySchema?: Joi.AnySchema;
+}
+
+export interface ValidatedRequestData {
+    body: any;
+    params: any;
+    query: any;
+}
+
+export interface APIResponse {
+    status: HttpStatusCode;
+    body?: any;
 }
