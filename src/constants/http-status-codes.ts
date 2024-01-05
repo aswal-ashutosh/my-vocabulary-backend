66660f7bfae594d892c45c4431ea9e0e20581e01
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-05 15:34:50 +0530

    add enum for http status codes

---
+++ src/constants/http-status-codes.ts
@@ -0,0 +1,14 @@
+const enum HttpStatusCode {
+    OK = 200,
+    CREATED = 201,
+    BAD_REQUEST = 400,
+    UNAUTHORIZED = 401,
+    FORBIDDEN = 403,
+    NOT_FOUND = 404,
+    UNSUPPORTED_MEDIA_TYPE = 415,
+    UNPROCESSABLE_ENTITY = 422,
+    TOO_MANY_REQUESTS = 429,
+    INTERNAL_SERVER_ERROR = 500
+};
+
+export default HttpStatusCode;
