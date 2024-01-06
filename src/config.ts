5931bac9d5f0068afa91227ced6449425b4d886d
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-06 21:55:27 +0530

    add file to store all env configurations

---
+++ src/config.ts
@@ -0,0 +1,14 @@
+import dotEnv from "dotenv";
+
+dotEnv.config();
+
+const env = process.env;
+
+export default {
+    MONGO_DB_URI: env.MONGO_DB_URI as string,
+    MONGO_DB_NAME: env.MONGO_DB_NAME as string,
+    NODEMAILER_USER: env.NODEMAILER_USER as string,
+    NODEMAILER_PASSWORD: env.NODEMAILER_PASSWORD as string,
+    JWT_ACCESS_TOKEN_KEY: env.JWT_ACCESS_TOKEN_KEY as string,
+    JWT_REFRESH_TOKEN_KEY: env.JWT_REFRESH_TOKEN_KEY as string,
+} as const;
