fe21ada5f86c5405abd2799c02568bbae68da8fb
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-08 00:23:50 +0530

    add vocabulary routes

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes";
+import vocabularyRoute from "./vocabulary.routes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/vocabulary", vocabularyRoute);
 
 export default router;
