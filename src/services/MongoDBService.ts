96f05d88732e1bfe2a1c899d43d2d4e916b86ff2
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-08 00:17:31 +0530

    get mongo config from config file

--- src/services/MongoDBService.ts
+++ src/services/MongoDBService.ts
@@ -1,14 +1,13 @@
 import { Db, MongoClient } from "mongodb";
+import Config from '../constants/config';
 
 export default abstract class MongoDBService {
-    private static readonly MONGO_DB_URI: string = process.env.MONGO_DB_URI!;
-    private static readonly MONGO_DB_NAME: string = process.env.MONGO_DB_NAME!;
     private static _db: Db = null!;
     private static _client: MongoClient = null!;
 
     private static async init() {
-        MongoDBService._client = await MongoClient.connect(MongoDBService.MONGO_DB_URI);
-        MongoDBService._db = MongoDBService._client.db(MongoDBService.MONGO_DB_NAME);
+        MongoDBService._client = await MongoClient.connect(Config.MONGO_DB_URI);
+        MongoDBService._db = MongoDBService._client.db(Config.MONGO_DB_NAME);
     }
 
     public static async getDB() {
