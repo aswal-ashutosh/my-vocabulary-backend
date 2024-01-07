4087ed4606cf3eb642dbc710e0568c965fde9bb8
aswal-ashutosh <ashu.aswal.333@gmail.com>
2024-01-08 00:21:36 +0530

    add controller for vocabulary

---
+++ src/controllers/VocabularyController.ts
@@ -0,0 +1,42 @@
+import Joi from "joi";
+import Controller from "./Controller";
+import { Request, Response } from "express";
+import { Word } from "../models/mongo-models";
+import { APIResponse } from "../types";
+import MongoDBService from "../services/MongoDBService";
+import Collections from "../constants/mongodb-collections";
+import HttpStatusCode from "../constants/http-status-codes";
+
+export default class VocabularyController extends Controller {
+    constructor(req: Request, res: Response) {
+        super(req, res);
+    }
+
+    public async addWord() {
+        try {
+            const notEmptyStringSchema = Joi.string().not().empty();
+            const bodySchema = Joi.object({
+                word: notEmptyStringSchema.required(),
+                definitions: Joi.array().items(notEmptyStringSchema).min(1).required(),
+                sentences: Joi.array().items(notEmptyStringSchema).max(10).required(),
+                similarWords: Joi.array().items(notEmptyStringSchema).max(10).required(),
+                oppositeWords: Joi.array().items(notEmptyStringSchema).max(10).required(),
+            });
+            const { body: word } = await this.validateRequest({ bodySchema });
+            const email: string = this.userInfo()!.email;
+            const { status, body } = await VocabularyController.addWord(word, email);
+            this.sendResponse(status, body);
+        } catch (error: any) {
+            this.handleError(error);
+        }
+    }
+
+    private static async addWord(word: Word, email: string): Promise<APIResponse> {
+        const db = await MongoDBService.getDB();
+        const { insertedId } = await db
+            .collection<Word>(Collections.WORDS)
+            .insertOne({ ...word, createdBy: email, createdAt: new Date() });
+        word._id = insertedId;
+        return { status: HttpStatusCode.CREATED, body: word };
+    }
+}
